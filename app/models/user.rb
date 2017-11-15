b61635223a5ceaf3c37b619241b16948e66ab196
Aaron Rohrbacher and Mark Helt <aaron+mark@>
2017-11-15 12:50:31 -0800

    can login with username

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,14 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => {email: true, login: false}
+         :recoverable, :rememberable, :trackable, :validatable
+
+## THIS IS KEY FOR LOGGING IN WITH USERNAME INSTEAD OF EMAIL:
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
 end
