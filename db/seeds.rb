c8ce572327d2a44713695fce6234326c6c9db5c9
Aaron Rohrbacher and Mark Helt <aaron+mark@>
2017-11-15 17:01:54 -0800

    seed users and comments, style changes to homepage

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 User.destroy_all
 Post.destroy_all
+Comment.destry_all
 
 # post_list = [
 #   ["Rick is an old man", 1, "Rick is too old and drunk all the time"],
@@ -15,13 +16,19 @@ Post.destroy_all
 # ]
 
 user_list = [
-  ["mark@mark.com", "Mark", "111111"],
-  ["aaron@aaro.com", "Aaron", "111111"],
-  ["javi@javi.com", "Javi", "111111"]
+  ["mark@mark.com", "Mark", "password"],
+  ["aaron@aaro.com", "Aaron", "password"],
+  ["javi@javi.com", "Javi", "password"]
 ]
 
 user_list.each do |email, username, password|
   User.create!(email: email, username: username, password: password)
+end
+
+20.times do |index|
+  User.create!(email: Faker::Internet.email,
+                        username: Faker::GameOfThrones.character,
+                        password: "password")
 end
 
 50.times do |index|
@@ -30,3 +37,14 @@ end
                         user_id: Faker::Number.between(User.first.id, User.last.id)
                         )
 end
+
+50.times do |index|
+  Comment.create!(content: Faker::Hipster.paragraph,
+                        user_id: Faker::Number.between(User.first.id, User.last.id),
+                        post_id: Faker::Number.between(Post.first.id, Post.last.id)
+                        )
+end
+
+p "Created #{User.count} users"
+p "Created #{Post.count} posts"
+p "Created #{Comment.count} comments"
